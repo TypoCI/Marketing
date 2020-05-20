0c22e37a040011328634a82b8be4928b8a447dd2
Mike Rogers <me@mikerogers.io>
2020-05-20 12:06:46 +0100

    Adding in HTML compression

--- config.rb
+++ config.rb
@@ -56,4 +56,23 @@ set :markdown, fenced_code_blocks: true, smartypants: true, tables: true, footno
 
 configure :build do
   activate :asset_hash, ignore: %r{^(images|uploads)/.*}
+
+  activate :minify_html do |html|
+    html.remove_multi_spaces        = true   # Remove multiple spaces
+    html.remove_comments            = true   # Remove comments
+    html.remove_intertag_spaces     = false  # Remove inter-tag spaces
+    html.remove_quotes              = false  # Remove quotes
+    html.simple_doctype             = false  # Use simple doctype
+    html.remove_script_attributes   = false  # Remove script attributes
+    html.remove_style_attributes    = true   # Remove style attributes
+    html.remove_link_attributes     = true   # Remove link attributes
+    html.remove_form_attributes     = false  # Remove form attributes
+    html.remove_input_attributes    = false  # Remove input attributes
+    html.remove_javascript_protocol = true   # Remove JS protocol
+    html.remove_http_protocol       = false  # Remove HTTP protocol
+    html.remove_https_protocol      = false  # Remove HTTPS protocol
+    html.preserve_line_breaks       = false  # Preserve line breaks
+    html.simple_boolean_attributes  = true   # Use simple boolean attributes
+    html.preserve_patterns          = nil    # Patterns to preserve
+  end
 end
