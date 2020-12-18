31276d75fedb5173fdc7f398f1de1be2b4844b59
Mike Rogers <me@mikerogers.io>
2020-12-18 12:07:30 +0000

    Updating JS packages to latest

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+
 const concurrently = require('concurrently');
 
 // By default, configure Bridgetown to use port 4001 so Browsersync can use 4000
@@ -8,6 +9,7 @@ const port = 4001
 // Concurrently
 /////////////////
 concurrently([
+  { command: "yarn webpack-dev", name: "Webpack", prefixColor: "yellow"},
   { command: "sleep 4; yarn serve --port " + port, name: "Bridgetown", prefixColor: "green"},
   { command: "sleep 8; yarn sync", name: "Live", prefixColor: "blue"}
 ], {
